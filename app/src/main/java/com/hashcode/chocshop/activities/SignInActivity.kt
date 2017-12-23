b38c757fdd0f526a6efdb62808c0f49f9dadf953
oluwaleke <hashilekky@gmail.com>
2017-12-23 12:14:43 +0100

    finished setting up sign in

--- app/src/main/java/com/hashcode/chocshop/activities/SignInActivity.kt
+++ app/src/main/java/com/hashcode/chocshop/activities/SignInActivity.kt
@@ -1,24 +1,25 @@
 package com.hashcode.chocshop.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.hashcode.chocshop.BuildConfig
 import com.hashcode.chocshop.R
-import java.util.*
-import java.util.Arrays.asList
-
+import java.util.Arrays
 
 
 class SignInActivity : AppCompatActivity() {
     private val RC_SIGN_IN = 123
-
+    val auth = FirebaseAuth.getInstance()!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        var auth = FirebaseAuth.getInstance()
         if(auth.currentUser != null){
 
         }
@@ -35,5 +36,32 @@ class SignInActivity : AppCompatActivity() {
                             .build(),
                     RC_SIGN_IN)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
+            if(resultCode == Activity.RESULT_OK){
+//                startActivity(Next Activity)
+                finish()
+                return
+            }
+            else {
+                if(response == null){
+//                    showSnackbar(R.string.sign_in_cancelled)
+                    return
+                }
+                if(response.errorCode == ErrorCodes.NO_NETWORK){
+//                    showSnackbar(R.string.no_internet_connection)
+                    return
+                }
+                if(response.errorCode == ErrorCodes.UNKNOWN_ERROR){
+//                    showSnackbar(R.string.unknown_error)
+                    return
+                }
+            }
+        }
+//        showSnackbar(R.string.unknown_sign_in_response)
     }
 }
